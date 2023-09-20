b08c25613cbd7fd0c6d0e626103e9c9fd59310a5
sunisok1 <sunisok1@gmail.com>
2023-09-20 15:33:59 +0800

    尖刺陷阱的制作

--- Actor/Player.gd
+++ Actor/Player.gd
@@ -1,5 +1,7 @@
 extends KinematicBody2D
 
+onready var sprite = $Sprite
+
 export var run_speed = 350
 export var jump_speed = -1000
 export var gravity = 2500
@@ -16,10 +18,20 @@ func get_input():
 		velocity.y = jump_speed
 	if right:
 		velocity.x += run_speed
+		sprite.scale.x = 1
 	if left:
 		velocity.x -= run_speed
+		sprite.scale.x = -1
 
 func _physics_process(delta):
 	velocity.y += gravity * delta
 	get_input()
 	velocity = move_and_slide(velocity, Vector2(0, -1))
+
+
+func _on_Area2D_area_entered(area):
+	PlayerDie()
+
+func PlayerDie():
+	print("die")
+	get_tree().reload_current_scene()
